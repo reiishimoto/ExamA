d491c9ccdf6764be3050ad77e87bdac916f8da0c
knz2474493 <knz2474493@stu.o-hara.ac.jp>
2025-04-25 10:42:23 +0900

    改定。

--- src/dao/TestListStudentDao.java
+++ src/dao/TestListStudentDao.java
@@ -14,37 +14,45 @@ public class TestListStudentDao extends Dao {
 
 	private String baseSql = "SELECT subject.name, subject.cd, test.no, test.point FROM test JOIN subject ON test.subject_cd = subject.cd WHERE test.student_no = ?;";
 
-	private TestListStudent postFilter(ResultSet rs) throws SQLException {
-		TestListStudent testListStudent = new TestListStudent();
+	private List<TestListStudent> postFilter(ResultSet rs) throws SQLException {
+		List<TestListStudent> list = new ArrayList<>();
 
+		TestListStudent testListStudent = new TestListStudent();
 		testListStudent.setSubjectName(rs.getString("subject.name"));
 		testListStudent.setSubjectCd(rs.getString("subject.cd"));
 		testListStudent.setNum(rs.getInt("test.no"));
 		testListStudent.setPoint(rs.getInt("test.point"));
 
-		return testListStudent;
+		list.add(testListStudent);
+
+		return list;
 	}
 
+	/**
+	 * Studentインスタンスを受け取り、当該studentのテスト結果をリストとして返却する。studentがnullであればnullを返す
+	 * @param student 該当のStudentインスタンス
+	 * @return 件数0を含め、クエリに成功した場合はListを返す。はず。例外発生なく失敗した場合はnullを返す
+	 */
 	public List<TestListStudent> filter (Student student) {
-		List<TestListStudent> results = new ArrayList<>();
-		try (Connection con = getConnection();
-			 PreparedStatement ps = con.prepareStatement(baseSql)) {
+		if(student == null) {
+			return null;
+		} else {
+			try (Connection con = getConnection();
+				 PreparedStatement ps = con.prepareStatement(baseSql)) {
 
-			ps.setString(1, student.getNo());
-			ResultSet rs = ps.executeQuery();
+				ps.setString(1, student.getNo());
+				ResultSet rs = ps.executeQuery();
 
-			while (rs.next()) {
-				results.add(postFilter(rs));
+				return postFilter(rs);
+
+			} catch (SQLException e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
+			} catch (Exception e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
 			}
-
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		} catch (Exception e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
 		}
-
-		return results;
+		return null;
 	}
 }
